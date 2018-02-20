b513efec5b46c9157a63f7dec14bdf09f76fc49a
shemgeek <shemnashon@gmail.com>
2018-02-21 00:10:26 +0300

    Initial commit

---
+++ components/HelloWorld.js
@@ -0,0 +1,10 @@
+import React from 'react'
+const HelloWorld = () => {
+  return (
+    <div className='hello-world'>
+      <h1>Hello React</h1>
+      <p>React project starts here...</p>
+    </div>
+  )
+}
+export default HelloWorld
