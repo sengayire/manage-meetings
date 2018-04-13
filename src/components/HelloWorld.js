9b5457612c1efbc4c4b1b09bf9726b6fb64d75f4
dennisja <dennisjjagwe@gmail.com>
2018-04-13 16:14:21 +0300

    Setup webpack and babel Add ide and log files to the .gitignore file Add  presets env and stage-0 to enable using of latest syntax in .babelrc Add instructions for updating the etc/hosts file to run the dev server Write the initial app configurations in webpack.config.js Write code to test whether the new class syntax works in src/components/HelloWorld.js Add assets folder to house the styles and logos

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,10 +1,26 @@
 import React from 'react'
-const HelloWorld = () => {
-  return (
-    <div className='hello-world'>
-      <h1>Hello React</h1>
-      <p>React project starts here...</p>
-    </div>
-  )
+import '../assets/styles/index.scss'
+import '../assets/styles/styles.css'
+
+class HelloWorld extends React.Component {
+  state = {
+    clicks: 0,
+  }
+
+  incrementClicks = ()=>{
+    this.setState(({clicks})=>({clicks: clicks+1}))
+  }
+
+  render () {
+    const { clicks } = this.state;
+    return (
+      <div className='hello-world'>
+        <h1>Hello MRM</h1>
+        <p>Meeting Room project starts here...</p>
+        <button className="hello" onClick={this.incrementClicks}>Clicked {clicks} times</button>
+      </div>
+    )
+  }
 }
+
 export default HelloWorld
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,42 @@
-var webpack = require('webpack');
-var path = require('path')
+const webpack = require('webpack')
+const path = require('path')
 
 module.exports = {
-  entry: path.resolve(__dirname, 'app'),
+  mode: 'development',
+  entry: path.resolve(__dirname, 'app.js'),
   output: {
-  path: __dirname + '/dist',
-  publicPath: '/',
-  filename: 'bundle.js'
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
   },
 
   devServer: {
-    contentBase: path.resolve(__dirname, 'public')
-},
+    contentBase: path.resolve(__dirname, 'public'),
+    compress: true,
+    open: true,
+    public: 'mrm-dev.andela.com:8080'
+  },
 
-module: {
-  loaders: [
-    {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
-    {test: /(\.css)$/, loaders: ['style-loader', 'css-loader']}
-]
-}
-
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: ['babel-loader'] },
+      { test: /(\.css)$/, use: ['style-loader', 'css-loader'] },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' }, // create style npdes from js strings
+          { loader: 'css-loader' }, // translate css into commonjs
+          { loader: 'sass-loader' } // compiles sass to css
+        ]
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      }
+    ]
+  }
 }
