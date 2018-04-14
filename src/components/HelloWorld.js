7a5f624a913e338bc9c1d31f5866df8178c034be
dennisja <dennisjjagwe@gmail.com>
2018-04-14 15:16:46 +0300

    [Chore #156719192] Add functionality that uses class variables to test whether everything works fine

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -3,21 +3,36 @@ import '../assets/styles/index.scss'
 import '../assets/styles/styles.css'
 
 class HelloWorld extends React.Component {
-  state = {
-    clicks: 0,
+  static initialState = {
+    clicks: 0
   }
 
-  incrementClicks = ()=>{
-    this.setState(({clicks})=>({clicks: clicks+1}))
+  state = HelloWorld.initialState
+
+  incrementClicks = () => {
+    this.setState(({ clicks }) => ({ clicks: clicks + 1 }))
+  }
+
+  resetClicks = () => {
+    this.state.clicks > 0 && this.setState(HelloWorld.initialState)
   }
 
   render () {
-    const { clicks } = this.state;
+    const { clicks } = this.state
     return (
       <div className='hello-world'>
         <h1>Hello MRM</h1>
         <p>Meeting Room project starts here...</p>
-        <button className="hello" onClick={this.incrementClicks}>Clicked {clicks} times</button>
+        <button className='hello' onClick={this.incrementClicks}>
+          Clicked {clicks} times
+        </button>
+        <button
+          onClick={this.resetClicks}
+          style={{ marginLeft: 10 }}
+          disabled={clicks <= 0}
+        >
+          Reset Counts
+        </button>
       </div>
     )
   }
