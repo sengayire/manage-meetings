96c23f34aa07a3f70187ab370d541c56ffff7a42
Bamuleseyo Gideon <gideonbamuleseyo@gmail.com>
2018-11-15 10:26:28 +0300

    chore(maintainability): clean codebase and improve maintainability (#124)
    - remove code smells from the codebase
    
    [Delivers #161834340]

--- src/containers/AnalyticsActivity.js
+++ src/containers/AnalyticsActivity.js
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
 import '../assets/styles/analyticsActivity.scss';
 import UsersIcon from '../assets/images/Group.svg';
+import meetings from '../utils/nonPersistentData';
 
 export class AnalyticsActivity extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      meetings: meetings(),
+    };
   }
+
+  meetingsData = date =>
+    (
+      <div className="activity">
+        <div className="activity-day">{date}</div>
+        {this.state.meetings.map(meeting => (
+          <div key={meeting.meetingId} className="activity-info-row">
+            <div className="title">
+              <div>{meeting.meetingTitle}</div>
+              <div className="time">{meeting.duration}</div>
+            </div>
+            <div className="room">{meeting.bookedRoom}</div>
+            {meeting.status.status === 'started' ? (
+              <div className="status">
+                <div className="started">
+                  Started <bdi>{meeting.status.startTime}</bdi>
+                </div>
+                <div className="ended">
+                  Ended <bdi>{meeting.status.endTime}</bdi>
+                </div>
+              </div>
+            ) : (
+              <div className="status">
+                <div className="cancelled">Cancelled</div>
+              </div>
+              )}
+
+            <div className="participants">
+              <img src={UsersIcon} alt="" />
+              <span>{meeting.participants}</span>
+            </div>
+          </div>
+
+        ))}
+      </div>
+    );
+
   render() {
     return (
       <div>
@@ -16,164 +56,8 @@ export class AnalyticsActivity extends Component {
           <div className="status-heang">Status</div>
           <div className="participant-heading">Participants</div>
         </div>
-
-        <div className="activity">
-          <div className="activity-day">Today</div>
-
-          <div className="activity-info-row">
-            <div className="title">
-              <div>Travela Backlog Grooming Started</div>
-              <div className="time">11:35 am - 12:30 pm</div>
-            </div>
-            <div className="room">Cognito</div>
-            <div className="status">
-              <div className="started">
-                Started <bdi>11:40am</bdi>
-              </div>
-              <div className="ended">
-                Ended <bdi>11:40am</bdi>
-              </div>
-            </div>
-            <div className="participants">
-              <img src={UsersIcon} alt="" />
-              <span>20</span>
-            </div>
-          </div>
-
-          <div className="activity-info-row">
-            <div className="title">
-              <div>Converge Sprint Planning Cancelled</div>
-              <div className="time">09:30 am - 10:30 pm</div>
-            </div>
-            <div className="room">Ol Karia</div>
-            <div className="status">
-              <div className="cancelled">Cancelled</div>
-            </div>
-            <div className="participants">
-              <img src={UsersIcon} alt="" />
-              <span>11</span>
-            </div>
-          </div>
-
-          <div className="activity-info-row">
-            <div className="title">
-              <div>Olamide Jolomi Sync</div>
-              <div className="time">09:30 am - 10:30 pm</div>
-            </div>
-            <div className="room">Amara</div>
-            <div className="status">
-              <div className="started">
-                Started <bdi>02:30pm</bdi>
-              </div>
-              <div className="ended">
-                Ended <bdi>03:00pm</bdi>
-              </div>
-            </div>
-            <div className="participants">
-              <img src={UsersIcon} alt="" />
-              <span>2</span>
-            </div>
-          </div>
-
-          <div className="activity-info-row">
-            <div className="title">
-              <div>Activo Backlog Grooming</div>
-              <div className="time">11:35 am - 12:30 pm</div>
-            </div>
-            <div className="room">Cairo</div>
-            <div className="status">
-              <div className="started">
-                Started <bdi>11:40am</bdi>
-              </div>
-              <div className="ended">
-                Ended <bdi>11:40am</bdi>
-              </div>
-            </div>
-            <div className="participants">
-              <img src={UsersIcon} alt="" />
-              <span>18</span>
-            </div>
-          </div>
-        </div>
-
-        <div className="activity">
-          <div className="activity-day">Friday October 6</div>
-
-          <div className="activity-info-row">
-            <div className="title">
-              <div>Travela Backlog Grooming Started</div>
-              <div className="time">11:35 am - 12:30 pm</div>
-            </div>
-            <div className="room">Cognito</div>
-            <div className="status">
-              <div className="started">
-                Started <bdi>11:40am</bdi>
-              </div>
-              <div className="ended">
-                Ended <bdi>11:40am</bdi>
-              </div>
-            </div>
-            <div className="participants">
-              <img src={UsersIcon} alt="" />
-              <span>17</span>
-            </div>
-          </div>
-
-          <div className="activity-info-row">
-            <div className="title">
-              <div>Converge Sprint Planning Cancelled</div>
-              <div className="time">09:30 am - 10:30 pm</div>
-            </div>
-            <div className="room">Ol Karia</div>
-            <div className="status">
-              <div className="cancelled">Cancelled</div>
-            </div>
-            <div className="participants">
-              <img src={UsersIcon} alt="" />
-              <span>12</span>
-            </div>
-          </div>
-
-          <div className="activity-info-row">
-            <div className="title">
-              <div>Olamide Jolomi Sync</div>
-              <div className="time">09:30 am - 10:30 pm</div>
-            </div>
-            <div className="room">Amara</div>
-            <div className="status">
-              <div className="started">
-                Started <bdi>02:30pm</bdi>
-              </div>
-              <div className="ended">
-                Ended <bdi>03:00pm</bdi>
-              </div>
-            </div>
-            <div className="participants">
-              <img src={UsersIcon} alt="" />
-              <span>2</span>
-            </div>
-          </div>
-
-          <div className="activity-info-row">
-            <div className="title">
-              <div>Activo Backlog Grooming</div>
-              <div className="time">11:35 am - 12:30 pm</div>
-            </div>
-            <div className="room">Cairo</div>
-            <div className="status">
-              <div className="started">
-                Started <bdi>11:40am</bdi>
-              </div>
-              <div className="ended">
-                Ended <bdi>11:40am</bdi>
-              </div>
-            </div>
-            <div className="participants">
-              <img src={UsersIcon} alt="" />
-              <span>15</span>
-            </div>
-          </div>
-        </div>
+        {this.meetingsData('Today')}
+        {this.meetingsData('Friday October 6')}
       </div>
     );
   }
---
+++ src/utils/nonPersistentData.js
@@ -0,0 +1,52 @@
+const meetings = () => [
+  {
+    meetingId: 1234,
+    meetingTitle: 'Travela Backlog Grooming Start',
+    duration: '11:35 am - 12:30 pm',
+    bookedRoom: 'Cognito',
+    status: {
+      status: 'started',
+      startTime: '11:40am',
+      endTime: '11:40am',
+    },
+    participants: 20,
+  },
+  {
+    meetingId: 55,
+    meetingTitle: 'Olamide Jolomi Sync',
+    duration: '09:30 am - 10:30 pm',
+    bookedRoom: 'Amara',
+    status: {
+      status: 'started',
+      startTime: '11:40am',
+      endTime: '11:40am',
+    },
+    participants: 2,
+  },
+
+  {
+    meetingId: 1222,
+    meetingTitle: 'Activo Backlog Grooming',
+    duration: '11:35 am - 12:30 pm',
+    bookedRoom: 'Amara',
+    status: {
+      status: 'started',
+      startTime: '11:40am',
+      endTime: '11:40am',
+    },
+    participants: 18,
+  },
+  {
+    meetingId: 1216889,
+    meetingTitle: 'Converge Sprint Planning Cancelled',
+    duration: '09:30 am - 10:30 pm',
+    bookedRoom: 'Ol Karia',
+    status: {
+      status: 'Cancelled',
+      startTime: '11:40am',
+      endTime: '11:40am',
+    },
+    participants: 18,
+  },
+];
+export default meetings;
