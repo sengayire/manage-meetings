491d45aee0e3cd453b811c9bb06b720489afcb3b
lauragift21 <laurandidi21@gmail.com>
2019-01-02 10:47:04 +0100

    bug(settings): Fix page refresh error on settings page
    - Fix page refresh error on settings page
    
    [Fixes #162786541]

--- src/components/roomFeedback/RoomFeedbackResponseList.js
+++ src/components/roomFeedback/RoomFeedbackResponseList.js
@@ -1,12 +1,12 @@
 import React, { Fragment } from 'react';
 import RoomFeedbackResponse from './RoomFeedbackResponse';
-import { NavBar } from '../../components';
 import '../../../src/assets/styles/roomFeedbackResponseList.scss';
 import '../../../src/assets/styles/feedbackContainer.scss';
 import roomFeedbackResponse from '../../fixtures/roomFeedbackResponse';
 import RoomFeedbackCard from './RoomFeedbackResponseCard';
 import BackArrow from '../../components/commons/BackArrow';
 import ROUTES from '../../utils/routes';
+import TopMenu from '../navbars/TopMenu';
 
 // maps over the list of feedback and returns each one of them
 const roomFeedbackItems = roomFeedbackResponse.map(feedback => (
@@ -18,7 +18,7 @@ const renderExportButton = () => (
     <svg width="10px" height="10px" viewBox="0 0 433.5 433.5" className="svg-export">
       <path d="M395.25 153h-102V0h-153v153h-102l178.5 178.5L395.25 153zm-357 229.5v51h357v-51h-357z" />
     </svg>
-  Export
+    Export
   </span>
 );
 
@@ -66,7 +66,7 @@ const renderCard = () => {
  */
 const RoomFeedbackResponseList = () => (
   <Fragment>
-    <NavBar route={ROUTES.roomResponses} />
+    <TopMenu />
     <div className="feedback-container">
       <BackArrow redirectUri={ROUTES.feedback} />
       <div className="feedback-card ">
--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -1,21 +1,13 @@
 import React, { Fragment } from 'react';
-import PropTypes from 'prop-types';
 import TopNav from '../components/navbars/TopNav';
 import TopMenu from '../components/navbars/TopMenu';
 
-const NavBar = ({ route }) => (
-  <Fragment>
+const NavBar = () => (
+  <Fragment >
     <TopMenu />
-    {route !== '/response' && <TopNav />}
-  </Fragment>
+    <TopNav />
+  </Fragment >
 );
 
-NavBar.propTypes = {
-  route: PropTypes.string,
-};
-
-NavBar.defaultProps = {
-  route: '',
-};
 export default NavBar;
 
