4911d6a12266ac8a5fb3c52df8b0d8322e01d2e1
okoro emeka <32361413+okoroemeka@users.noreply.github.com>
2019-04-10 07:36:01 +0100

    ch(setup): add parent title (#335)
    add parent title to the formated data
    
    [Delivers #165218943]

--- src/utils/formatSetupData.js
+++ src/utils/formatSetupData.js
@@ -9,10 +9,27 @@ const formatData = (dataArray, structureData) => {
     parentId: structureData.parentId,
     tag: structureData.tag,
     quantity: dataArray.children.length + 1,
+    parentTitle: structureData.parentTitle,
     children: [...dataArray.children, structureData],
   };
   formatedData.children.sort((a, b) => a.position - b.position);
   return formatedData;
+};
+/**
+ * @returns {parent} string
+ * @param {array} sortedData
+ * @param {object} child
+ */
+const getparentTitle = (sortedData, child) => {
+  let parent;
+  sortedData.forEach((element) => {
+    if (child.level - 1 && element.level === child.level - 1 && child.parentId === element.id) {
+      parent = element;
+      return parent;
+    }
+    return null;
+  });
+  return parent;
 };
 /**
  * @param {array} arrayToSort
@@ -31,12 +48,15 @@ const orderByLevel = (dataArray) => {
   const newArray = [];
 
   sortedData.forEach((element) => {
+    const newObj = Object.assign({}, element);
+    const parentTitle = getparentTitle(sortedData, element);
+    newObj.parentTitle = parentTitle ? parentTitle.name : '';
     newArray[element.level - 1] =
       newArray[element.level - 1] !== undefined
-        ? formatData(newArray[element.level - 1], element)
+        ? formatData(newArray[element.level - 1], newObj, sortedData)
         : {
           id: '',
-          children: [element],
+          children: [newObj],
           parentId: element.parentId,
           tag: element.tag,
           quantity: 1,
@@ -44,4 +64,5 @@ const orderByLevel = (dataArray) => {
   });
   return newArray;
 };
+
 export default orderByLevel;
