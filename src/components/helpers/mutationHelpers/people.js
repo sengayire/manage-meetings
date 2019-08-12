5f3b7ec0bf5ef608240f0f79cd3fef5b3da4f7dd
ruganda <mubaruganda@gmail.com>
2019-08-12 10:33:52 +0300

    fix(office structure): Enable refectching of an office structure.

--- src/components/helpers/mutationHelpers/people.js
+++ src/components/helpers/mutationHelpers/people.js
@@ -2,6 +2,8 @@
 import apolloClient from '../../../utils/ApolloClient';
 import { INVITE_PERSON_MUTATION, CHANGE_USER_LOCATION_MUTATION } from '../../../graphql/mutations/People';
 import { decodeTokenAndGetUserData } from '../../../utils/Cookie';
+import GET_ALL_LEVELS from '../../../graphql/queries/Levels';
+
 
 const invitePersonMutation = async (email, client = apolloClient) => {
   await client
@@ -21,6 +23,10 @@ const changeUserLocation = async (locationId, queryEmail, client = apolloClient)
       mutation: CHANGE_USER_LOCATION_MUTATION,
       name: 'changeUserLocation',
       variables: { email, locationId },
+      refetchQueries: [{
+        query: GET_ALL_LEVELS,
+      },
+      ],
     });
 
   return data;
